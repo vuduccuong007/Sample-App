19286cd13b87e0cd9498747738de183569713f9f
vuduccuong007 <vuduccuong007@gmail.com>
2020-12-08 19:43:17 +0700

    Chapter 6

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,11 @@
 class User < ApplicationRecord
-  validate :name :email,  presence: true , length: {minimum: 3}
-  validate :name :email,  presence: true, length: {minimum: 5}
+  validates :name, presence: true, length: { maximum: 50 }
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  validates :email, presence: true, length: { maximum: 255 },
+                   format: { with: VALID_EMAIL_REGEX },
+                   uniqueness: { case_sensitive: false }
+
+  has_secure_password
+
+  validates :password, presence: true, length: { minimum: 6 }
 end
