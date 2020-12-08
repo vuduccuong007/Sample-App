89ce882c5037789ab5204905b2295b2521b25f11
vuduccuong007 <vuduccuong007@gmail.com>
2020-12-08 21:51:01 +0700

    Chap7-08/12

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,12 @@ Rails.application.routes.draw do
   get 'static_pages/home'
   get 'static_pages/help'
   get 'static_pages/about'
-  resources :users
+  get 'users/new', to: 'users#new'
+  get 'users/:id', to: 'users#show'
+  get 'users/', to: 'users#index'
+
+  post 'users', to: 'users#create'
+  # get 'users/edit', to: 'users#edit'
+  # get 'users/create', to: 'users#create'
+  # resources :users
 end
