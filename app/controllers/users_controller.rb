b138fcefee3f88aa17368639e5faf19e96e5e197
Vu Duc Cuong <vuduccuong007@gmail.com>
2020-12-15 01:50:39 +0700

    Chapter7_home

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -19,6 +19,8 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
+      flash[:success] = "Welcome to the Sample App!"
+      redirect_to user_url(@user)
     else
       render 'new'
     end
--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,8 @@
 Rails.application.routes.draw do
   root 'static_pages#home'
-  get 'static_pages/home'
-  get 'static_pages/help'
-  get 'static_pages/about'
-  get 'users/new', to: 'users#new'
-  get 'users/:id', to: 'users#show'
-  get 'users/', to: 'users#index'
-
-  post 'users', to: 'users#create'
-  # get 'users/edit', to: 'users#edit'
-  # get 'users/create', to: 'users#create'
-  # resources :users
+  get '/help', to: 'static_pages#help'
+  get '/about', to: 'static_pages#about'
+  get '/contact', to: 'static_pages#contact'
+  get '/signup', to: 'users#new'
+  resources :users
 end
