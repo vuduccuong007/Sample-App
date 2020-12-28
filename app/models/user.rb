5a2e77cd3a480cdc62ddedd4e162dc2ff0ec5fe1
Vu Duc Cuong <vuduccuong007@gmail.com>
2020-12-29 01:00:50 +0700

    chap 14 home

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,11 @@
 class User < ApplicationRecord
   has_many :microposts, dependent: :destroy
+  has_many :active_relationships, class_name: "Relationship",
+           foreign_key: "follower_id", dependent: :destroy
+  has_many :passive_relationships, class_name: "Relationship",
+           foreign_key: "followed_id", dependent: :destroy
+  has_many :following, through: :active_relationships, source: :followed
+  has_many :followers, through: :passive_relationships, source: :follower
   attr_accessor :remember_token
   before_save { self.email = email.downcase }
   validates :name, presence: true, length: { maximum: 50 }
@@ -23,7 +29,17 @@ class User < ApplicationRecord
     BCrypt::Password.new(remember_digest).is_password?(remember_token)
   end
   def feed
-    Micropost.where("user_id = ?", id)
+    part_of_feed = "relationships.follower_id = :id or microposts.user_id = :id"
+    Micropost.joins(user: :followers).where(part_of_feed, { id: id })
+  end
+  def follow(other_user)
+    following << other_user
+  end
+  def unfollow(other_user)
+    following.delete(other_user)
+  end
+  def following?(other_user)
+    following.include?(other_user)
   end
   class << self
     def digest(string)
